23923ddc52e110eaee30f7c34e421de6ed6da297
Prithvi Patthi <87157790+Prithvi999@users.noreply.github.com>
2022-11-15 18:55:01 -0600

    Add files via upload

---
+++ Boilerplate_code.py
@@ -0,0 +1,16 @@
+#Importing flask module in the project
+from flask import Flask
+
+#Create an object of the Flask class
+app = Flask(__name__)
+
+#The route() function of the Flask class 
+@app.route("/")
+
+#‘/’ URL is bound with first_flask function.
+def Home():
+    name = ""
+    age = ""
+
+#run the application on local server
+app.run()
